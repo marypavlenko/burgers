5b6a7c3475440ea1e8638c2d43bbc161aae9af9e
Maria <mary.pavlenko.ab@gmail.com>
2019-02-01 16:47:56 +0300

    Proportion block form

--- main.js
+++ main.js
@@ -1,135 +1,75 @@
+
+
 ymaps.ready(init);
 
-var placemarks = [
-    {
-        letitude: 55.75903557,
-        longitude: 37.62551350,
-        hintContent: '<div class="map__hint">st. Nikolskaya, 12</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Mr. Burgers. The best natural burgers here',
-            'farshburger.ru',
-            '8 (495) 258-42-05',
-            'Open now:  10AM–12AM',
-            '</div>'
-        ]
-    },
-
-    {
-        letitude: 55.73859107,
-        longitude: 37.41100450,
-        hintContent: '<div class="map__hint">st. Yartsevskaya, 19</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Mr. Burgers. The best natural burgers here',
-            'farshburger.ru',
-            '8 (495) 258-42-05',
-            'Open now:  10AM–12AM',
-            '</div>'
-        ]
-    },
-
-    {
-        letitude: 55.77904857,
-        longitude: 37.58860150,
-        hintContent: '<div class="map__hint">st. Lesnaya, 9</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Mr. Burgers. The best natural burgers here',
-            'farshburger.ru',
-            '8 (495) 258-42-05',
-            'Open now:  10AM–12AM',
-            '</div>'
-        ]
-    },
-
-    {
-        letitude: 55.74079315,
-        longitude: 37.85534700,
-        hintContent: '<div class="map__hint">Blvd. Enthusiasts, d. 2</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Mr. Burgers. The best natural burgers here',
-            'farshburger.ru',
-            '8 (495) 258-42-05',
-            'Open now:  10AM–12AM',
-            '</div>'
-        ]
-    },
-
-    {
-        letitude: 55.72903657,
-        longitude: 37.62379750,
-        hintContent: '<div class="map__hint">B. Serpukhovskaya ul., 2</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Mr. Burgers. The best natural burgers here',
-            'farshburger.ru',
-            '8 (495) 258-42-05',
-            'Open now:  10AM–12AM',
-            '</div>'
-        ]
-    }
-],
-
-geoObjects = [];
-
-
-function init () {
-    var map = new ymaps.Map('map',{
-        center: [55.75399400, 37.62209300],
-        zoom: 14,
-        controls: ['zoomControl'],
-        behavior: ['drag']
-    });
-
-    for (var i = 0; i < placemarks.length; i++) {
-        geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+var map,
+    placemarks = [
         {
-            hintContent: placemarks[i].hintContent,
-            balloonContent: placemarks[i].balloonContent.join('')
+
+            letitude: 59.97,
+            longitude: 30.3,
+            hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+            balloonContent: [
+                '<div class="map__balloon">',
+                '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+                'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 21',
+                '</div>'
+            ]
         },
-
         {
-            iconLayout: 'default#image',
-            iconImageHref: 'image/icon/logo.svg',
-            iconImageSize: [46, 57],
-            iconImageOffset: [-23, -57]
-        });
-    }
+            letitude: 59.94,
+            longitude: 30.25,
+            hintContent: '<div class="map__hint">Малый проспект В. О., д. 64к1</div>',
+            balloonContent: [
+                '<div class="map__balloon">',
+                '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+                'Самые вкусные бургеры у нас! Заходите по адресу: уМалый проспект В. О., д. 64к1',
+                '</div>'
+            ]
+        },
+        {
+            letitude: 59.93,
+            longitude: 30.34,
+            hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+            balloonContent: [
+                '<div class="map__balloon">',
+                '<img class="map__burger-img" src="img/burger.png" alt="Бургер"/>',
+                'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+                '</div>'
+            ]
+        }];
 
-    var clusterer = new ymaps.Clusterer({
-        clusterIcons: [
-            {
-                href: 'image/icon/logo.svg',
-                size: [100, 100],
-                offset: [-50, -50]
-            }
-        ],
-        clusterIconContentLayout: null
+function init() {
+    map = new ymaps.Map('map', {
+        center: [59.95, 30.32],
+        zoom: 12,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
     });
 
-    map.geoObjects.add(clusterer);
-    clusterer.add(geoObjects);
+    placemarks.forEach(function(obj) {
+        placemark = new ymaps.Placemark([obj.latitude, obj.longitude], {
+            hintContent: obj.hintContent,
+            balloonContent: obj.balloonContent.join('')
+        }, {
+            // iconLayout: 'default#image',
+            // iconImageHref: 'img/map-marker.png',
+            // iconImageSize: [46, 57],
+            // iconImageOffset: [-23, -57]
+        });
+
+        map.geoObjects.add(placemark);
+    });
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//Без цикла и перебора
-
-//     var placemark1 = new ymaps.Placemark([55.75903557, 37.62551350],{
+// ymaps.ready(init);
+//
+// var placemarks = [
+//     {
+//         letitude: 55.75903557,
+//         longitude: 37.62551350,
 //         hintContent: '<div class="map__hint">st. Nikolskaya, 12</div>',
 //         balloonContent: [
 //             '<div class="map__balloon">',
@@ -138,91 +78,217 @@ function init () {
 //             '8 (495) 258-42-05',
 //             'Open now:  10AM–12AM',
 //             '</div>'
-//         ].join('')
+//         ]
+//     },
+//
+//     {
+//         letitude: 55.73859107,
+//         longitude: 37.41100450,
+//         hintContent: '<div class="map__hint">st. Yartsevskaya, 19</div>',
+//         balloonContent: [
+//             '<div class="map__balloon">',
+//             'Mr. Burgers. The best natural burgers here',
+//             'farshburger.ru',
+//             '8 (495) 258-42-05',
+//             'Open now:  10AM–12AM',
+//             '</div>'
+//         ]
+//     },
+//
+//     {
+//         letitude: 55.77904857,
+//         longitude: 37.58860150,
+//         hintContent: '<div class="map__hint">st. Lesnaya, 9</div>',
+//         balloonContent: [
+//             '<div class="map__balloon">',
+//             'Mr. Burgers. The best natural burgers here',
+//             'farshburger.ru',
+//             '8 (495) 258-42-05',
+//             'Open now:  10AM–12AM',
+//             '</div>'
+//         ]
+//     },
+//
+//     {
+//         letitude: 55.74079315,
+//         longitude: 37.85534700,
+//         hintContent: '<div class="map__hint">Blvd. Enthusiasts, d. 2</div>',
+//         balloonContent: [
+//             '<div class="map__balloon">',
+//             'Mr. Burgers. The best natural burgers here',
+//             'farshburger.ru',
+//             '8 (495) 258-42-05',
+//             'Open now:  10AM–12AM',
+//             '</div>'
+//         ]
+//     },
+//
+//     {
+//         letitude: 55.72903657,
+//         longitude: 37.62379750,
+//         hintContent: '<div class="map__hint">B. Serpukhovskaya ul., 2</div>',
+//         balloonContent: [
+//             '<div class="map__balloon">',
+//             'Mr. Burgers. The best natural burgers here',
+//             'farshburger.ru',
+//             '8 (495) 258-42-05',
+//             'Open now:  10AM–12AM',
+//             '</div>'
+//         ]
+//     }
+// ],
+//
+// geoObjects = [];
+//
+//
+// function init () {
+//     var map = new ymaps.Map('map',{
+//         center: [55.75399400, 37.62209300],
+//         zoom: 14,
+//         controls: ['zoomControl'],
+//         behavior: ['drag']
+//     });
+//
+//     for (var i = 0; i < placemarks.length; i++) {
+//         geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+//         {
+//             hintContent: placemarks[i].hintContent,
+//             balloonContent: placemarks[i].balloonContent.join('')
 //         },
 //
 //         {
-//             iconLayout: 'default#image',
-//             iconImageHref: 'image/icon/logo.svg',
-//             iconImageSize: [46, 57],
-//             iconImageOffset: [-23, -57]
-//         },
-//
-//     var placemark2 = new ymaps.Placemark([55.73859107, 37.41100450],{
-//             hintContent: '<div class="map__hint">st. Yartsevskaya, 19</div>',
-//             balloonContent: [
-//                 '<div class="map__balloon">',
-//                 'Mr. Burgers. The best natural burgers here',
-//                 'farshburger.ru',
-//                 '8 (495) 259-67-29',
-//                 'Open now:  10AM–12AM',
-//                 '</div>'
-//             ].join('')
-//         },
-//         {
-//             iconLayout: 'default#image',
-//             iconImageHref: 'image/icon/logo.svg',
-//             iconImageSize: [46, 57],
-//             iconImageOffset: [-23, -57]
-//         },
-//
-//     var placemark3 = new ymaps.Placemark([55.77904857, 37.58860150],{
-//             hintContent: '<div class="map__hint">st. Lesnaya, 9</div>',
-//             balloonContent: [
-//                 '<div class="map__balloon">',
-//                 'Mr. Burgers. The best natural burgers here',
-//                 'farshburger.ru',
-//                 '8 (495) 263-87-00',
-//                 'Open now:  10AM–12AM',
-//                 '</div>'
-//             ].join('')
-//         },
-//         {
-//             iconLayout: 'default#image',
-//             iconImageHref: 'image/icon/logo.svg',
-//             iconImageSize: [46, 57],
-//             iconImageOffset: [-23, -57]
-//         },
-//
-//     var placemark4 = new ymaps.Placemark([55.74079315, 37.85534700],{
-//             hintContent: '<div class="map__hint">Blvd. Enthusiasts, d. 2</div>',
-//             balloonContent: [
-//                 '<div class="map__balloon">',
-//                 'Mr. Burgers. The best natural burgers here',
-//                 'farshburger.ru',
-//                 '8 (495) 278-50-24',
-//                 'Open now:  10AM–12AM',
-//                 '</div>'
-//             ].join('')
-//         },
-//         {
-//             iconLayout: 'default#image',
-//             iconImageHref: 'image/icon/logo.svg',
-//             iconImageSize: [46, 57],
-//             iconImageOffset: [-23, -57]
-//         },
-//
-//     var placemark5 = new ymaps.Placemark([55.72903657, 37.62379750],{
-//             hintContent: '<div class="map__hint">B. Serpukhovskaya ul., 2</div>',
-//             balloonContent: [
-//                 '<div class="map__balloon">',
-//                 'Mr. Burgers. The best natural burgers here',
-//                 'farshburger.ru',
-//                 '8 (495) 207-94-81',
-//                 'Open now:  10AM–12AM',
-//                 '</div>'
-//             ].join('')
-//         },
-//         {
-//             iconLayout: 'default#image',
-//             iconImageHref: 'image/icon/logo.svg',
-//             iconImageSize: [46, 57],
-//             iconImageOffset: [-23, -57]
+//             // iconLayout: 'default#image',
+//             // // iconImageHref: 'image/icon/logo.svg',
+//             // iconImageSize: [46, 57],
+//             // iconImageOffset: [-23, -57]
 //         });
+//     }
 //
-//     map.geoObjects.add(placemark1);
-//     map.geoObjects.add(placemark2);
-//     map.geoObjects.add(placemark3);
-//     map.geoObjects.add(placemark4);
-//     map.geoObjects.add(placemark5);
+//     // var clusterer = new ymaps.Clusterer({
+//     //     clusterIcons: [
+//     //         {
+//     //             // href: 'image/icon/logo.svg',
+//     //             size: [100, 100],
+//     //             offset: [-50, -50]
+//     //         }
+//     //     ],
+//     //     clusterIconContentLayout: null
+//     };
+//
+//     map.geoObjects.add(clusterer);
+//     clusterer.add(geoObjects);
 // }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //Без цикла и перебора
+//
+// //     var placemark1 = new ymaps.Placemark([55.75903557, 37.62551350],{
+// //         hintContent: '<div class="map__hint">st. Nikolskaya, 12</div>',
+// //         balloonContent: [
+// //             '<div class="map__balloon">',
+// //             'Mr. Burgers. The best natural burgers here',
+// //             'farshburger.ru',
+// //             '8 (495) 258-42-05',
+// //             'Open now:  10AM–12AM',
+// //             '</div>'
+// //         ].join('')
+// //         },
+// //
+// //         {
+// //             iconLayout: 'default#image',
+// //             iconImageHref: 'image/icon/logo.svg',
+// //             iconImageSize: [46, 57],
+// //             iconImageOffset: [-23, -57]
+// //         },
+// //
+// //     var placemark2 = new ymaps.Placemark([55.73859107, 37.41100450],{
+// //             hintContent: '<div class="map__hint">st. Yartsevskaya, 19</div>',
+// //             balloonContent: [
+// //                 '<div class="map__balloon">',
+// //                 'Mr. Burgers. The best natural burgers here',
+// //                 'farshburger.ru',
+// //                 '8 (495) 259-67-29',
+// //                 'Open now:  10AM–12AM',
+// //                 '</div>'
+// //             ].join('')
+// //         },
+// //         {
+// //             iconLayout: 'default#image',
+// //             iconImageHref: 'image/icon/logo.svg',
+// //             iconImageSize: [46, 57],
+// //             iconImageOffset: [-23, -57]
+// //         },
+// //
+// //     var placemark3 = new ymaps.Placemark([55.77904857, 37.58860150],{
+// //             hintContent: '<div class="map__hint">st. Lesnaya, 9</div>',
+// //             balloonContent: [
+// //                 '<div class="map__balloon">',
+// //                 'Mr. Burgers. The best natural burgers here',
+// //                 'farshburger.ru',
+// //                 '8 (495) 263-87-00',
+// //                 'Open now:  10AM–12AM',
+// //                 '</div>'
+// //             ].join('')
+// //         },
+// //         {
+// //             iconLayout: 'default#image',
+// //             iconImageHref: 'image/icon/logo.svg',
+// //             iconImageSize: [46, 57],
+// //             iconImageOffset: [-23, -57]
+// //         },
+// //
+// //     var placemark4 = new ymaps.Placemark([55.74079315, 37.85534700],{
+// //             hintContent: '<div class="map__hint">Blvd. Enthusiasts, d. 2</div>',
+// //             balloonContent: [
+// //                 '<div class="map__balloon">',
+// //                 'Mr. Burgers. The best natural burgers here',
+// //                 'farshburger.ru',
+// //                 '8 (495) 278-50-24',
+// //                 'Open now:  10AM–12AM',
+// //                 '</div>'
+// //             ].join('')
+// //         },
+// //         {
+// //             iconLayout: 'default#image',
+// //             iconImageHref: 'image/icon/logo.svg',
+// //             iconImageSize: [46, 57],
+// //             iconImageOffset: [-23, -57]
+// //         },
+// //
+// //     var placemark5 = new ymaps.Placemark([55.72903657, 37.62379750],{
+// //             hintContent: '<div class="map__hint">B. Serpukhovskaya ul., 2</div>',
+// //             balloonContent: [
+// //                 '<div class="map__balloon">',
+// //                 'Mr. Burgers. The best natural burgers here',
+// //                 'farshburger.ru',
+// //                 '8 (495) 207-94-81',
+// //                 'Open now:  10AM–12AM',
+// //                 '</div>'
+// //             ].join('')
+// //         },
+// //         {
+// //             iconLayout: 'default#image',
+// //             iconImageHref: 'image/icon/logo.svg',
+// //             iconImageSize: [46, 57],
+// //             iconImageOffset: [-23, -57]
+// //         });
+// //
+// //     map.geoObjects.add(placemark1);
+// //     map.geoObjects.add(placemark2);
+// //     map.geoObjects.add(placemark3);
+// //     map.geoObjects.add(placemark4);
+// //     map.geoObjects.add(placemark5);
+// // }
