62646dfb19544294c3485f961869fe1b6d6fab39
Maria <mary.pavlenko.ab@gmail.com>
2019-03-14 23:06:43 +0300

    Add appearance menu

--- main.js
+++ main.js
@@ -170,7 +170,6 @@ function darkMenu() {
     openBtnDark.on('click', function (event) {
         event.preventDefault();
         menuDark.fadeIn();
-        menuDark.css('display', 'block');
     });
 
     closeBtnDark.on('click', function (event) {
